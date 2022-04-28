60012d3604200ed6ad2f70ff8f589d5c516cb509
marcelo <marcelo@contassconsultoria.com.br>
2022-04-27 22:21:57 -0300

    iniciando com angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ExtratoComponent } from './extrato/extrato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTransferenciaComponent,
+    ExtratoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    providers: [{provide: LOCALE_ID, useValue: 'en-US' }]
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
