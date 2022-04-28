06ab9f5e1752bb7df2d16e37d9acd916c844d89d
marcelo <marcelo@contassconsultoria.com.br>
2022-04-28 11:58:51 -0300

    integrando form com a api

--- src/app/services/transferencia.service.ts
+++ src/app/services/transferencia.service.ts
@@ -1,4 +1,7 @@
+import { Transferencia } from './../models/transferencia.model';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -6,8 +9,9 @@ import { Injectable } from '@angular/core';
 export class TransferenciaService {
 
   private listaTransferencia: any[];
+  private url = 'http://localhost:3000/transferencias';
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
     this.listaTransferencia = [];
   }
 
@@ -15,9 +19,13 @@ export class TransferenciaService {
     return this.listaTransferencia;
   }
 
-  adicionar(transferencia: any){
+  todas(): Observable<Transferencia[]>{
+    return this.httpClient.get<Transferencia[]>(this.url);
+  }
+
+  adicionar(transferencia: any): Observable<Transferencia>{
     this.hidratar(transferencia);
-    this.listaTransferencia.push(transferencia);
+    return this.httpClient.post<Transferencia>(this.url, transferencia);
   }
 
   private hidratar(transferencia: any){
